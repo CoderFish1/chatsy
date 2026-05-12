28bca00ee39b853a442323d4a624d1dd37820ff6
CoderFish1 <shreypandey22005@gmail.com>
2026-05-12 05:56:54 +0530

    initial api endpoints created

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import { configDotenv } from 'dotenv';
 import { chats } from './data/data.js';
 
 const app = express()
+configDotenv();
 
 app.get('/',(req,res) =>{
     res.send('API is Running')
@@ -11,4 +13,14 @@ app.get('/api/chat', (req,res)=>{
     res.send(chats)
 })
 
-app.listen(5000,console.log('Server started on PORT 5000'))
+app.get('/api/chat/:id', (req, res)=>{
+    // console.log(req.params.id)
+    const singleChat = chats.find((c)=>
+       c._id === req.params.id
+    );
+    res.send(singleChat);
+})
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT,console.log(`Server started on PORT ${PORT}`))   
