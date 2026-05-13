9b2c7fd8ff1d074c945415ba5a87149011dd3638
CoderFish1 <shreypandey22005@gmail.com>
2026-05-13 22:48:45 +0530

    frontend setup(chakra ui + tailwind)

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { Button, HStack } from "@chakra-ui/react";
+
+function App() {
+
+  return (
+    <>
+    <div className='text-3xl'>hello</div>
+        <Button>Click me</Button>
+        <Button>Click me</Button>
+    </>
+  );
+}
+
+export default App
